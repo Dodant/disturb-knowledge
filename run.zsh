e55c7c6d3b2f0ddb4205221f6ac0a8f1850405cd
Dodant <ohho0506@gmai.com>
2022-04-27 22:18:36 +0900

    add files

---
+++ run.zsh
@@ -0,0 +1,3 @@
+python cifar100_kd_with_pl_original.py -student 20 -t 10.0 -alpha 0.1 -batch 256 -epochs 500 -repeat 3
+
+python cifar100_kd_with_pl_original.py -student 32 -t 10.0 -alpha 0.1 -batch 256 -epochs 500 -repeat 3
